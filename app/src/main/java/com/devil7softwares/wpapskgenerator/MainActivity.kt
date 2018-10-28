d742b43ddfff83b43c612aa18297f422c9d214e4
Devil7DK <dineshthangavel47@gmail.com>
2018-10-28 07:40:01 +0530

    Show Toast Messages if SSID/Passpharase is Empty

--- app/src/main/java/com/devil7softwares/wpapskgenerator/MainActivity.kt
+++ app/src/main/java/com/devil7softwares/wpapskgenerator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
@@ -33,6 +34,15 @@ class MainActivity : AppCompatActivity() {
         val id = findViewById<EditText>(R.id.txtSSID)
         val pass = findViewById<EditText>(R.id.txtPassPhrase)
         val psk = findViewById<EditText>(R.id.txtPSK)
+
+        if (id.text.isNullOrBlank()) {
+            Toast.makeText(this, R.string.message_id_empty, Toast.LENGTH_SHORT).show()
+            return@OnClickListener
+        }
+        if (pass.text.isNullOrBlank()) {
+            Toast.makeText(this, R.string.message_pass_empty, Toast.LENGTH_SHORT).show()
+            return@OnClickListener
+        }
 
         val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
         val keySpec = PBEKeySpec(pass.text.toString().toCharArray(), id.text.toString().toByteArray(), 4096, 256)
