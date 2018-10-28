cc4051118f7fb1688823f4763c3152c2900fd661
Devil7DK <dineshthangavel47@gmail.com>
2018-10-28 07:31:05 +0530

    Implement 'GeneratePSK' Function

--- app/src/main/java/com/devil7softwares/wpapskgenerator/MainActivity.kt
+++ app/src/main/java/com/devil7softwares/wpapskgenerator/MainActivity.kt
@@ -2,12 +2,20 @@ package com.devil7softwares.wpapskgenerator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val generate = findViewById<Button>(R.id.btnGeneratePSK)
+        generate.setOnClickListener(generatePSK)
     }
 
     fun stringToHex(str: ArrayList<Byte>) =
@@ -20,5 +28,22 @@ class MainActivity : AppCompatActivity() {
                                 .toString(16)
                                 .padStart(2, '0')
                     }
-    
+
+    private val generatePSK = View.OnClickListener {
+        val id = findViewById<EditText>(R.id.txtSSID)
+        val pass = findViewById<EditText>(R.id.txtPassPhrase)
+        val psk = findViewById<EditText>(R.id.txtPSK)
+
+        val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+        val keySpec = PBEKeySpec(pass.text.toString().toCharArray(), id.text.toString().toByteArray(), 4096, 256)
+        val secretKey = secretKeyFactory.generateSecret(keySpec)
+        val bytes = secretKey.encoded
+
+        val list = ArrayList<Byte>(bytes.size)
+        for (b in bytes) {
+            list.add(b)
+        }
+
+        psk.setText(stringToHex(list))
+    }
 }
